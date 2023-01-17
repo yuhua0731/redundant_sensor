673834f4cf7dd3b035dbcdbcaf7e2a60fa7c4bfa
yuhua <yuhua364@gmail.com>
2023-01-17 16:52:36 +0800

    update script

--- redundant_sensor_status.py
+++ redundant_sensor_status.py
@@ -5,6 +5,9 @@ import json
 import collections
 from enum import Enum, auto
 
+COUNT_RETRY = False
+retrying = [False]
+
 sensor_name = {
     0: 'PLS_A',
     2: 'PLS_B',
@@ -38,16 +41,17 @@ class task_result(Enum):
 # end_time = parse_time('2022-12-04T13:11:00.000000+0800')
 # duration = datetime.timedelta(hours=2)
 test_time = [
-    ['2022-12-10T20:01:00.000000+0800', '2022-12-10T20:13:00.000000+0800'],
-    ['2022-12-10T20:13:00.000000+0800', '2022-12-10T20:24:00.000000+0800'],
-    ['2022-12-10T20:24:00.000000+0800', '2022-12-10T20:35:00.000000+0800'],
-    ['2022-12-10T20:43:00.000000+0800', '2022-12-10T20:53:00.000000+0800'],
-    ['2022-12-10T20:53:00.000000+0800', '2022-12-10T21:05:00.000000+0800'],
-    ['2022-12-10T21:05:00.000000+0800', '2022-12-10T21:15:00.000000+0800']
+    # ['2022-12-25T13:40:00.000000+0800', '2022-12-25T13:54:00.000000+0800'],
+    ['2022-12-29T09:00:00.000000+0800', '2022-12-29T18:00:00.000000+0800'],
+    # ['2022-12-11T14:18:00.000000+0800', '2022-12-11T14:39:00.000000+0800'],
+    # ['2022-12-11T16:55:00.000000+0800', '2022-12-11T17:13:00.000000+0800'],
+    # ['2022-12-11T17:27:00.000000+0800', '2022-12-11T17:36:00.000000+0800'],
+    # ['2022-12-11T17:37:00.000000+0800', '2022-12-11T17:50:00.000000+0800'],
+    # ['2022-12-11T17:55:00.000000+0800', '2022-12-11T18:04:00.000000+0800']
 ]
 
 # these log files contain infomation about sensors' status
-files = glob.glob('./221211/shuttle-mqtt-*.log/shuttle-mqtt-*.log*', recursive=True)
+files = glob.glob('./221229_log/*.log', recursive=True)
 
 cnt = {
     'load_start': 0,
@@ -67,19 +71,30 @@ task_info = {
     'unload_valid_flag': False,
 }
 
+def reset():
+    pass
+
 # robot_sequence = collections.defaultdict(list)
 load_sequence = collections.defaultdict(int)
 unload_sequence = collections.defaultdict(int)
 seq = []
 
 global pre_status, valid_seq_flag
-pre_status = 0
+pre_status = (0, 0, 0)
 valid_seq_flag = True
 
 get_status = lambda status : status & (1 << 0 | 1 << 2 | 1 << 6 | 1 << 7)
 get_status_bin = lambda status : ' & '.join(list(filter(bool, [[name, ''][status & (1 << bitmask) == 0] for bitmask, name in sensor_name.items()])))
 parse_time = lambda x : datetime.datetime.strptime(x, "%Y-%m-%dT%H:%M:%S.%f%z")
 test_time = [list(map(parse_time, i)) for i in test_time]
+
+def convert_plain_log_to_json(s):
+    s = s.split(' TRACE: 	')
+    ret = {
+        "message": s[1],
+        "timestamp": s[0]
+    }
+    return ret
 
 def parse_data(s):
     uid = int(re.search(r'uid: [0-9]+', s).group(0).split(': ')[-1])
@@ -94,12 +109,14 @@ def load_start(pre):
     cnt['load_start'] += 1
     task_info['load_result'] = -1
     task_info['load_valid_flag'] = True
+    retrying[0] = False
     return [pre]
 
 def unload_start(pre):
     cnt['unload_start'] += 1
     task_info['unload_result'] = -1
     task_info['unload_valid_flag'] = True
+    retrying[0] = False
     return [pre]
 
 def load_complete(valid, seq):
@@ -108,9 +125,10 @@ def load_complete(valid, seq):
     else:
         cnt['load_end'] += 1
         curr_seq = get_seq(seq, task_info['load_end_time'])
-        if curr_seq[-1] != 0: 
-            curr_seq.append(0)
+        # if curr_seq[-1] != 0: 
+            # curr_seq.append(0)
             # print(seq)
+        # if curr_seq == [0, 1]: print(curr_seq, seq[-1])
         curr_seq.append(task_info['load_result'])
         load_sequence[tuple(curr_seq)] += 1
 
@@ -120,9 +138,9 @@ def unload_complete(valid, seq):
     else:
         cnt['unload_end'] += 1
         curr_seq = get_seq(seq, task_info['unload_end_time'])
-        if curr_seq[-1] != 0: 
-            curr_seq.append(0)
-            # print(seq)
+        # if curr_seq[-1] != 0: 
+        #     curr_seq.append(0)
+        #     print(seq)
         curr_seq.append(task_info['unload_result'])
         unload_sequence[tuple(curr_seq)] += 1
 
@@ -136,11 +154,38 @@ def append_status(data, phase):
         seq.append((uid, tick, sensor_status))
 
 for f in files:
+    """resetting state
+    """
+    reset()
+    for k in cnt.keys():
+        cnt[k] = 0
+    
+    task_info = {
+        'load_end_time': 0,
+        'unload_end_time': 0,
+        'load_result': -1,
+        'unload_result': -1,
+        'load_valid_flag': False,
+        'unload_valid_flag': False,
+    }
+    seq = []
+    pre_status = (0, 0, 0)
+    valid_seq_flag = True
+    load_sequence = collections.defaultdict(int)
+    unload_sequence = collections.defaultdict(int)
+    """resetting finished
+    """
+
     robot_id = re.search(r'[F-G][0-9AS]{5}', f).group(0)
     with open(f, "r") as content:
         phase = robot_status.INIT
         for line in content:
-            data = json.loads(line)
+            if 'TRACE' not in line: continue
+            try:
+                data = json.loads(line)
+            except:
+                data = convert_plain_log_to_json(line)
+
             if 'sensor_state' in line:
                 pre_status = parse_data(data['message'])
             t = parse_time(data['timestamp'])
@@ -148,26 +193,31 @@ for f in files:
                 if start <= t <= end:
                     match phase:
                         case robot_status.INIT:
-                            if '0_START' in line:
+                            if 'START_LOAD' in line:
                                 # start load phase
                                 seq = load_start(pre_status)
                                 phase = robot_status.LOADING
                         case robot_status.LOADING:
                             if 'sensor_state' in line:
                                 append_status(data, phase)
-                            if 'load_state' in line:
+                            if ' load_state' in line:
                                 task_info['load_end_time'] = int(re.search(r'os_tick_count: [0-9]+', line).group(0).split(': ')[-1])
                                 task_info['load_result'] = int(re.search(r'load_state: [0-9]+', line).group(0).split(': ')[-1])
-                            if '0_START' in line:
-                                # previous load sequence is invalid
-                                load_complete(valid_seq_flag, seq)
+                            if 'START_LOAD' in line:
                                 # this is a load retry from application, just ignore
-                                # start load phase
-                                seq = load_start(pre_status)
+                                if COUNT_RETRY:
+                                    load_complete(valid_seq_flag, seq)
+                                    # start load phase
+                                    seq = load_start(pre_status)
+                                else:
+                                    if not retrying[0]:
+                                        load_complete(valid_seq_flag, seq)
+                                        retrying[0] = True
                                 phase = robot_status.LOADING
-                            if 'UNLOAD_1_START' in line:
+                            if 'START_UNLOAD' in line:
                                 # current load phase is finished
-                                load_complete(valid_seq_flag, seq)
+                                if COUNT_RETRY or not retrying[0]:
+                                    load_complete(valid_seq_flag, seq)
                                 
                                 # start unload phase
                                 seq = unload_start(pre_status)
@@ -175,18 +225,24 @@ for f in files:
                         case robot_status.UNLOADING:
                             if 'sensor_state' in line:
                                 append_status(data, phase)
-                            if 'unload_state' in line:
+                            if ' unload_state' in line:
                                 task_info['unload_end_time'] = int(re.search(r'os_tick_count: [0-9]+', line).group(0).split(': ')[-1])
                                 task_info['unload_result'] = int(re.search(r'unload_state: [0-9]+', line).group(0).split(': ')[-1])
-                            if 'UNLOAD_1_START' in line:
-                                # previous unload sequence is invalid
-                                cnt['invalid_unload'] += 1
-                                # start load phase
-                                seq = unload_start(pre_status)
+                            if 'START_UNLOAD' in line:
+                                # this is an unload retry from application, just ignore
+                                if COUNT_RETRY:
+                                    unload_complete(valid_seq_flag, seq)
+                                    # start unload phase
+                                    seq = unload_start(pre_status)
+                                else:
+                                    if not retrying[0]:
+                                        unload_complete(valid_seq_flag, seq)
+                                        retrying[0] = True
                                 phase = robot_status.UNLOADING
-                            if '0_START' in line:
+                            if 'START_LOAD' in line:
                                 # current unload phase is finished
-                                unload_complete(valid_seq_flag, seq)
+                                if COUNT_RETRY or not retrying[0]:
+                                    unload_complete(valid_seq_flag, seq)
                                 
                                 # start load phase
                                 seq = load_start(pre_status)
@@ -205,17 +261,20 @@ for f in files:
                 unload_complete(valid_seq_flag, seq)
             case _:
                 print(f"unknown status: {phase} at {data['timestamp']}")
+    print(robot_id)
 
-print('load')
-for k, v in sorted(load_sequence.items(), key=lambda x: -x[1]):
-    n = len(k)
-    k, result = k[:n - 1], k[-1]
-    print([get_status_bin(i) for i in k] + [task_result(result).name], v)
+    print('load')
+    for k, v in sorted(load_sequence.items(), key=lambda x: -x[1]):
+        n = len(k)
+        k, result = k[:n - 1], k[-1]
+        print([task_result(result).name] + [v] + [get_status_bin(i) for i in k])
 
-print('unload')
-for k, v in sorted(unload_sequence.items(), key=lambda x: -x[1]):
-    n = len(k)
-    k, result = k[:n - 1], k[-1]
-    print([get_status_bin(i) for i in k] + [task_result(result).name], v)
+    print('unload')
+    for k, v in sorted(unload_sequence.items(), key=lambda x: -x[1]):
+        n = len(k)
+        k, result = k[:n - 1], k[-1]
+        print([task_result(result).name] + [v] + [get_status_bin(i) for i in k])
 
-print(cnt)
+    print(cnt)
+
+# 943CC680E874 943CC681873C 40F520BDB494 943CC6818528 943CC60536B0 AC67B2E896C0 98CDAC507358 943CC680D278 943CC680E090 943CC680EA28 943CC680D684 943CC680CD80 0CB81557A8F4 3083984E0460 943CC6816108 8CCE4E85A048 943CC6828968
